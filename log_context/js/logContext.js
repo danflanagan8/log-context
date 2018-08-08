69932824bb5ea708eb9286b1d47beb7083301d03
Dan Flanagan <dan.flanagan@manifest.com>
2018-08-08 16:19:41 -0500

    Added drupalSettings variable to make the test better and to make the logs more helpful.

--- log_context/js/logContext.js
+++ log_context/js/logContext.js
@@ -1,19 +1,21 @@
-(function ($) {
+(function ($, Drupal, drupalSettings) {
   Drupal.behaviors.logContext = {
-    attach: function(context) {
-
+    attach: function(context, drupalSettings) {
       $('body').once('context-log-test-info').each(function(){
         console.log('👓 Call Drupal.behaviors.logContext.test() to see if the page can handle a new element. 🔍');
       });
 
       console.log('Drupal\'s attachBehaviors function was called. The "context" variable is logged below.');
       console.log(context);
+      console.log('The "drupalSettings" object is logged below.');
+      console.log(drupalSettings);
+
     },
     test: function() {
       $('body').append('<div class="log-context-test">This element was added by the Log Context module as a test.</div>');
       var $tests = $('.log-context-test');
       var new_elem = $tests.get($tests.length - 1);
-      Drupal.attachBehaviors(new_elem);
+      Drupal.attachBehaviors(new_elem, drupalSettings);
     }
   }
-})(jQuery);
+})(jQuery, Drupal, drupalSettings);
